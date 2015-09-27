21c5bd4d3c52e1805393390177a9e80480040c6b
petrihakkinen <petrih3@gmail.com>
2015-09-27 12:17:51 +0300

    SD card read test.

---
+++ wav-player.ino
@@ -0,0 +1,69 @@
+/*
+  SD card read/write
+
+ This example shows how to read and write data to and from an SD card file
+ The circuit:
+ * SD card attached to SPI bus as follows:
+ ** MOSI - pin 11
+ ** MISO - pin 12
+ ** CLK - pin 13
+ ** CS - pin 4
+
+ created   Nov 2010
+ by David A. Mellis
+ modified 9 Apr 2012
+ by Tom Igoe
+
+ This example code is in the public domain.
+
+ */
+
+#include <SPI.h>
+#include <SD.h>
+
+File myFile;
+
+void setup()
+{
+	// Open serial communications and wait for port to open:
+	Serial.begin(9600);
+	while(!Serial)
+	{
+		// wait for serial port to connect. Needed for native USB port only
+	}
+
+
+	Serial.print("Initializing SD card...");
+
+	if(!SD.begin(4))
+	{
+		Serial.println("initialization failed!");
+		return;
+	}
+	Serial.println("initialization done.");
+
+	// open the file. note that only one file can be open at a time,
+	// re-open the file for reading:
+	myFile = SD.open("test.txt");
+	if(!myFile)
+	{
+		// if the file didn't open, print an error:
+		Serial.println("error opening test.txt");
+		return;
+	}
+
+	Serial.println("test.txt:");
+
+	// read from the file until there's nothing else in it:
+	while(myFile.available())
+	{
+		Serial.write(myFile.read());
+	}
+	// close the file:
+	myFile.close();
+}
+
+void loop()
+{
+	// nothing happens after setup
+}
